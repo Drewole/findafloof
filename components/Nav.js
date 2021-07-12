9974908d33ba94ca9015373cb95f45ccf237ea69
Drew Olsen <olsenj.andrew@gmail.com>
2021-07-12 12:48:59 -0500

    Changed navigation font size

--- components/Nav.js
+++ components/Nav.js
@@ -9,13 +9,13 @@ const Nav = () => {
     return (
         <Box as="nav" separator="">
             <Box as="ul" d="flex">
-                <Box ml="3" mr="3" as="li" fontSize="12px" color="purple.800">
+                <Box ml="3" mr="3" as="li" fontSize="xs" color="purple.800">
                     <Link href="/" ><a><HouseFill size={28} />Browse</a></Link>
                 </Box>
-                <Box ml="3" mr="3" as="li" fontSize="12px" color="purple.800">
+                <Box ml="3" mr="3" as="li" fontSize="xs" color="purple.800">
                     <Link href="/favorites" ><a><HeartFill size={28} />Fav's</a></Link>
                 </Box>
-                <Box ml="3" mr="3" as="li" fontSize="12px" color="purple.800">
+                <Box ml="3" mr="3" as="li" fontSize="xs" color="purple.800">
                     <Link href="/share" ><a><ShareFill size={28} />Share</a></Link>
                 </Box>
             </Box>
