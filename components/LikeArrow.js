c7bffd89c23686a2b95282c67962a434a4254e06
Drew Olsen <olsenj.andrew@gmail.com>
2021-08-08 00:16:02 -0500

    Broke some parts into components

---
+++ components/LikeArrow.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { CheckLg } from 'react-bootstrap-icons'
+import { Button, Box } from '@chakra-ui/react'
+
+const LikeArrow = ({ handleChoice }) => {
+    return (
+        <Button
+            colorScheme="green"
+            backgroundColor="green.300"
+            p={6}
+            right={35}
+            _hover={{ cursor: 'pointer' }}
+            zIndex={1}
+            height={70}
+            width={70}
+            onClick={() => handleChoice("right")}
+            borderStyle="solid"
+            _hover={{
+                cursor: 'pointer',
+                transform: 'scale(1.25)'
+            }}
+            position="relative"
+            borderColor="white"
+            borderWidth={4}
+            borderRadius="50%">
+            <Box as="span" position="relative" zIndex="2">
+                <CheckLg color="DarkGreen" size={30} />
+            </Box>
+        </Button>
+    )
+}
+
+export default LikeArrow
---
+++ components/PassArrow.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Button, Box } from '@chakra-ui/react'
+import { XLg } from 'react-bootstrap-icons'
+
+const PassArrow = ({ handleChoice, }) => {
+    return (
+        <Button
+            colorScheme="red"
+            left={35}
+            backgroundColor="red.300"
+            p={6}
+            height={70}
+            width={70}
+            zIndex={1}
+            borderStyle="solid"
+            _hover={{
+                cursor: 'pointer',
+                transform: 'scale(1.25)'
+            }}
+            position="relative"
+            borderColor="white"
+            borderWidth={4}
+            borderRadius="50%"
+            onClick={() => handleChoice("left")}
+        >
+            <Box as="span" position="relative" zIndex="2">
+                <XLg size={30} color="DarkRed" />
+            </Box>
+        </Button>
+    )
+}
+
+export default PassArrow
